aca9f8fc80c36deb2fd75252c99ff8e1af294ab8
stockbsd <stockbsd@gmail.com>
2014-03-21 09:18:34 +0800

    增加翻译文件

--- pq5one/main.py
+++ pq5one/main.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 from unone_ui import Ui_MainWindow
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -6,10 +6,32 @@ class MainWindow(QtWidgets.QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.actionNew.triggered.connect(self.filenew)
+        self.ui.actionOpen.triggered.connect(self.filexit)
+        self.ui.actionAbout.triggered.connect(self.about)
+        self.setWindowTitle(self.tr('Pyside'))
+
+    def filenew(self):
+        print('static method')
+        QtWidgets.QApplication.quit()
+
+    def about(self):
+        print('qApp method')
+        QtWidgets.qApp.quit()
+
+    def filexit(self):
+        print('instance()')
+        QtWidgets.QApplication.instance().quit()
 
 if __name__ == '__main__':
     import sys
     app = QtWidgets.QApplication(sys.argv)
+
+    tr = QtCore.QTranslator()
+    if tr.load('main.zh_CN'):
+        app.installTranslator(tr)
+
     mwnd = MainWindow()
     mwnd.show()
+
     sys.exit(app.exec_())
---
+++ pq5one/main.zh_CN.ts
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="unone.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>主窗口</translation>
+    </message>
+    <message>
+        <location filename="unone.ui" line="37"/>
+        <source>File</source>
+        <translation>文件</translation>
+    </message>
+    <message>
+        <location filename="unone.ui" line="173"/>
+        <source>Help</source>
+        <translation>帮助</translation>
+    </message>
+    <message>
+        <location filename="unone.ui" line="53"/>
+        <source>Edit</source>
+        <translation>编辑</translation>
+    </message>
+    <message>
+        <location filename="unone.ui" line="112"/>
+        <source>Tab 1</source>
+        <translation>看看</translation>
+    </message>
+    <message>
+        <location filename="unone.ui" line="122"/>
+        <source>Tab 2</source>
+        <translation>页面2</translation>
+    </message>
+    <message>
+        <location filename="unone.ui" line="141"/>
+        <source>New</source>
+        <translation>新建</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="12"/>
+        <source>Pyside</source>
+        <translation>汉语</translation>
+    </message>
+    <message>
+        <location filename="unone.ui" line="192"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="unone.ui" line="159"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="unone.ui" line="168"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="unone.ui" line="178"/>
+        <source>Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="unone.ui" line="183"/>
+        <source>Exit2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
