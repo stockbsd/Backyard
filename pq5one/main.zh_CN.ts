8d800645b1ddd8897929dbd5e4c06df219d1bf9a
stockbsd <stockbsd@gmail.com>
2014-03-24 09:48:14 +0800

    update UI

--- pq5one/main.zh_CN.ts
+++ pq5one/main.zh_CN.ts
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="zh_CN">
+<!DOCTYPE TS><TS version="2.0" language="zh_CN" sourcelanguage="">
 <context>
     <name>MainWindow</name>
     <message>
@@ -14,7 +13,7 @@
         <translation>文件</translation>
     </message>
     <message>
-        <location filename="unone.ui" line="173"/>
+        <location filename="unone.ui" line="185"/>
         <source>Help</source>
         <translation>帮助</translation>
     </message>
@@ -24,17 +23,17 @@
         <translation>编辑</translation>
     </message>
     <message>
-        <location filename="unone.ui" line="112"/>
+        <location filename="unone.ui" line="124"/>
         <source>Tab 1</source>
         <translation>看看</translation>
     </message>
     <message>
-        <location filename="unone.ui" line="122"/>
+        <location filename="unone.ui" line="134"/>
         <source>Tab 2</source>
         <translation>页面2</translation>
     </message>
     <message>
-        <location filename="unone.ui" line="141"/>
+        <location filename="unone.ui" line="153"/>
         <source>New</source>
         <translation>新建</translation>
     </message>
@@ -44,29 +43,34 @@
         <translation>汉语</translation>
     </message>
     <message>
-        <location filename="unone.ui" line="192"/>
+        <location filename="unone.ui" line="162"/>
         <source>Exit</source>
         <translation>退出</translation>
     </message>
     <message>
-        <location filename="unone.ui" line="159"/>
-        <source>About</source>
-        <translation>关于</translation>
-    </message>
-    <message>
-        <location filename="unone.ui" line="168"/>
+        <location filename="unone.ui" line="180"/>
         <source>Open</source>
         <translation>打开</translation>
     </message>
     <message>
-        <location filename="unone.ui" line="178"/>
+        <location filename="unone.ui" line="190"/>
         <source>Copy</source>
         <translation>拷贝</translation>
     </message>
     <message>
-        <location filename="unone.ui" line="183"/>
+        <location filename="unone.ui" line="195"/>
         <source>Exit2</source>
         <translation>退出吧</translation>
+    </message>
+    <message>
+        <location filename="unone.ui" line="171"/>
+        <source>About Pq5</source>
+        <translation>关于 PQ5</translation>
+    </message>
+    <message>
+        <location filename="unone.ui" line="204"/>
+        <source>Exit PQ5</source>
+        <translation>退出Pq5</translation>
     </message>
 </context>
 </TS>
